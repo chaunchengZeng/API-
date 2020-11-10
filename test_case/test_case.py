0dacccd0c03902f5684cb1de2fb2c274381654e6
chuanchengZeng <2015130039@email.szu.edu.cn>
2020-11-10 21:59:59 +0800

    [Add] 新增一个python用例文件

---
+++ test_case/test_case.py
@@ -0,0 +1,17 @@
+class Test(object):
+
+    def test_01(self):
+        print('This is test01')
+
+    def test_02(self):
+        print("This is test02")
+
+    def test_03(self):
+        print('This is test03')
+
+
+if __name__ == "__main__":
+    t = Test()
+    t.test_01()
+    t.test_02()
+    t.test_03()
